933af8bf990092d40d244c5c9dbf3fff3e0ba594
KSFlynn007 <kelseyflynnn@gmail.com>
2021-08-21 16:21:35 -0600

    sounds all working in scale order

--- js/piano.js
+++ js/piano.js
@@ -1,3 +1,8 @@
+// notes in order
+// G#, A, Bb, B, C, C#, D, Eb, E, F, F#, G
+// 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 221  
+
+
 window.addEventListener('keydown', checkKeyPress, false);
 
 function checkKeyPress(key)
@@ -5,31 +10,34 @@ function checkKeyPress(key)
     const soundItem = document.querySelector('.list-group-item');
     if(key.keyCode == "65"){
         // highlight list item
+        document.querySelector('#audioGSharp').play();
+    } else if (key.keyCode == "83"){
         document.querySelector('#audioA').play();
-    }
-    if(key.keyCode == "66"){
-        document.querySelector('#audioB').play();
-    }
-    if(key.keyCode == '86'){
+    } else if (key.keyCode == "68"){
         document.querySelector('#audioBb').play();
-    }
-    if(key.keyCode == '88'){
-        document.querySelector('#audioCSharp').play();
-    }
-    if(key.keyCode == '67'){
+    } else if (key.keyCode == "70"){
+        document.querySelector('#audioB').play();
+    } else if (key.keyCode =="71"){
         document.querySelector('#audioC').play();
+    } else if (key.keyCode == "72"){
+        document.querySelector('#audioCSharp').play();
+    } else if (key.keyCode == "74"){
+        document.querySelector('#audioD').play();
+    } else if (key.keyCode == "75"){
+        document.querySelector('#audioEb').play();
+    } else if (key.keyCode == "76"){
+        document.querySelector('#audioE').play();
+    } else if (key.keyCode == "186"){
+        document.querySelector('#audioF').play();
+    } else if(key.keyCode == "222"){
+        document.querySelector(`#audioFSharp`).play();
+    } else if(key.keyCode == "221"){
+        document.querySelector('#audioG').play();
     }
-
-    
 }
-// case here to alternate between keys?
-// if (key.keyCode == '65'){
-//     // play a
-//     // highlight list item on screen
-// }
-// if (keycode == 66){
-//     // play b
-// }
+
+
+
 
 
 
