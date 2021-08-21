dc1351707b2cff3ff7ecf9f1da87efa72e30854f
KSFlynn007 <kelseyflynnn@gmail.com>
2021-08-21 13:17:38 -0600

    correct event listener in place
    not yet finished

--- js/piano.js
+++ js/piano.js
@@ -1,9 +1,35 @@
-// const soundBtn = document.querySelector('.play-sound-btn');
-// let myAudio = document.querySelector('#audioA');
-// soundBtn.addEventListener('click', ()=>{
-//     myAudio.play();
-// })
+window.addEventListener('keydown', checkKeyPress, false);
 
-$("#audioA").keypress(function() {
-    console.log('Handler for a called');
-})
+function checkKeyPress(key)
+{
+    const soundItem = document.querySelector('.list-group-item');
+    if(key.keyCode == "65"){
+        // highlight list item
+        document.querySelector('#audioA').play();
+    }
+    if(key.keyCode == "66"){
+        document.querySelector('#audioB').play();
+    }
+    if(key.keyCode == '86'){
+        document.querySelector('#audioBb').play();
+    }
+    if(key.keyCode == '88'){
+        document.querySelector('#audioCSharp').play();
+    }
+    if(key.keyCode == '67'){
+        document.querySelector('#audioC').play();
+    }
+
+    
+}
+// case here to alternate between keys?
+// if (key.keyCode == '65'){
+//     // play a
+//     // highlight list item on screen
+// }
+// if (keycode == 66){
+//     // play b
+// }
+
+
+
